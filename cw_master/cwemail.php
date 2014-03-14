01c9f6b90941778df71aefab7a3636da63ef5576
kategol <kate.gollogly12@gmail.com>
2014-03-14 14:37:33 -0400

    Kate - editing form

---
+++ cw_master/cwemail.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'Mail.php';
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email  = $_POST['email'];
+$msg = $_POST['msg'];
+$is_body_html = true;
+
+echo send_email($fname, $lname, $email, $msg, $is_body_html);
+
+function send_email($fname, $lname, $email, $msg, $is_body_html = false) {
+   
+
+    $smtp = array();
+    
+    $smtp['host'] = 'capitalwaterfowlingc.netfirms.com';
+    $smtp['port'] = 587;
+    $smtp['auth'] = true;
+    $smtp['username'] = 'dustin@capitalwaterfowling.com';
+    $smtp['password'] = 'Password1';
+    
+    $mailer = Mail::factory('smtp',$smtp);
+
+    // Add the email address to the list of all recipients
+    $recipients = array();
+    $recipients[] = $to;
+
+    // Set the headers
+    $headers = array();
+    $headers['From'] = $from;
+    $headers['To']  = $to;
+    $headers['Subject'] = $subject;
+    if ($is_body_html) {
+        $headers['Content-type']  = 'text/html';
+    }
+
+    // Send the email
+
+    $result = $mailer->send($recipients, $headers, $body);
+    // Check the result and throw an error if one exists
+    return $result;
+}
+
+
+?>
